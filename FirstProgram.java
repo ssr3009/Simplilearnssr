58a56443f56a5280417a53def4a52382bf6b21ad
Sumit <sumit4u786@gmail.com>
2022-05-14 22:04:34 +0530

    First Code

---
+++ FirstProgram.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.util.Scanner;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+Scanner sc=new Scanner(System.in);
+        
+        System.out.print("Enter Name : ");
+        String name=sc.next();
+        
+        System.out.println("MYName is :"+name );
+        
+        System.out.println("Enter age:");
+        String age=sc.next();
+        System.out.println("my age is:"+ age);
+	}
+
+}
